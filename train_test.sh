237319033aadddfc9178603e3f8bddf60d90a093
ma dizhi <dizhima@gmail.com>
2023-03-29 18:17:33 -0400

    feat: update

--- train_test.sh
+++ train_test.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=3 python train_pp.py --dataset Synapse \
+CUDA_VISIBLE_DEVICES=3 python train.py --dataset Synapse \
 --cfg 'configs/swin_tiny_patch4_window7_224_lite.yaml' \
 --root_path 'datasets/Synapse' \
 --max_epochs 150   \
@@ -7,7 +7,7 @@ CUDA_VISIBLE_DEVICES=3 python train_pp.py --dataset Synapse \
 --base_lr 1e-4 \
 --batch_size 24
 
-CUDA_VISIBLE_DEVICES=3 python test_pp.py \
+CUDA_VISIBLE_DEVICES=3 python test.py \
 --dataset Synapse \
 --cfg configs/swin_tiny_patch4_window7_224_lite.yaml \
 --is_saveni --output_dir ./swinunet_pp_run17 --max_epoch 150  \
