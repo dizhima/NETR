c939efc5d6e3b2ac086bd9e0319a9ab392e4f703
ma dizhi <dizhima@gmail.com>
2023-03-30 13:47:35 -0400

    feat: modified bash file

--- train_test.sh
+++ train_test.sh
@@ -2,7 +2,7 @@ CUDA_VISIBLE_DEVICES=3 python train.py --dataset Synapse \
 --cfg 'configs/swin_tiny_patch4_window7_224_lite.yaml' \
 --root_path 'datasets/Synapse' \
 --max_epochs 150   \
---output_dir ./swinunet_pp_run17 \
+--output_dir model_out \
 --img_size 224 \
 --base_lr 1e-4 \
 --batch_size 24
@@ -10,5 +10,5 @@ CUDA_VISIBLE_DEVICES=3 python train.py --dataset Synapse \
 CUDA_VISIBLE_DEVICES=3 python test.py \
 --dataset Synapse \
 --cfg configs/swin_tiny_patch4_window7_224_lite.yaml \
---is_saveni --output_dir ./swinunet_pp_run17 --max_epoch 150  \
+--is_saveni --output_dir model_out --max_epoch 150  \
 --base_lr 1e-4 --img_size 224 --batch_size 24
---
+++ visualization.sh
@@ -0,0 +1,2 @@
+python visualization.py --dataset Synapse --cfg configs/swin_tiny_patch4_window7_224_lite.yaml \
+ --is_saveni --output_dir model_out --max_epoch 150 --base_lr 1e-4 --img_size 224 --batch_size 24
